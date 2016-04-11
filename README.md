5d3d53270f71771c97c1f97acad8b4aaeca25794
Oliver Binns <mail@oliverbinns.info>
2016-04-11 22:43:42 +0100

    Added placeholders for two pages of the site. Updated read me.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Alex-Dora-Wedding
+##A simple, responsive one page website created for my brother's wedding.
+
+The aim of the website is to give the guests information about the wedding. The site will be especially useful to the large number of guests who will be travelling from the UK to Greece for the wedding.
+
+The source code for this wedding is freely available in both meanings of the word (free speech/free beer).
+Feel free to use this code for your own purposes and edit where necessary, but please credit me where possible :)
+
+There are dependencies on both Twitter Bootstrap and phpUI.
+* (Twitter Bootstrap)[https://www.github.com/twbs/bootstrap]
+* (phpUI)[https://www.github.com/Oliver-Binns/phpUI]
+
+Both these dependencies should be copied into the root directory of the project.
+```
+git clone https://www.github.com/Oliver-Binns/Alex-Dora-Wedding.git
+cd Alex-Dora-Wedding
+git clone https://www.github.com/twbs/bootstrap.git
+git clone https://www.github.com/Oliver-Binns/phpUI.git
+```
--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 	include 'phpUI/autoloader.php';
+	use phpHTML\UICore\UIDiv;
+	use phpHTML\UICore\UIGlyphicon;
 	use phpHTML\UICore\UIPage\UINav;
 	use phpHTML\UICore\UILink;
 	use phpHTML\JSCore\JSObject;
- ?>
+	use phpHTML\UICore\UISection;
+
+?>
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.min.css">
@@ -16,11 +20,23 @@
 			echo new UINav('', [
 				new UILink('Our Story'),
 				new UILink('The Wedding Day'),
-				new UILink('Travel'),
+				new UILink('Travel','#travel'),
 				new UILink('Recommendations')
-			]) ?>				
-		
-		<?php 
+			], [], [], UINav::FIXED_TOP);
+
+			echo new UISection(['<h1>Alex & Dora</h1>'], 'background');
+
+			echo new UIDiv([
+				new UISection([], 'section0'),
+				'<a name="travel">',
+				new UISection([
+					new UIDiv([
+						new UIDiv([new UIGlyphicon(UIGlyphicon::PLANE), new UIGlyphicon(UIGlyphicon::ROAD), new UIGlyphicon(UIGlyphicon::PRINT_), new UIGlyphicon(UIGlyphicon::CAMERA)], 'icons')
+					], 'container')
+				], 'section1'),
+				'</a>'
+			], 'body-sections');
+
 			echo new JSObject('',JSObject::libraryLink('jquery', '2.2.3'));
         		echo new JSObject('','bootstrap/dist/js/bootstrap.min.js');
 		?>
