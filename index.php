855c830a5dc3f6043131c095084276ef2ca973ef
Oliver Binns <mail@oliverbinns.info>
2016-04-12 07:45:13 +0100

    Now using font awesome for transport icons.

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
 	use phpHTML\UICore\UILink;
 	use phpHTML\JSCore\JSObject;
 	use phpHTML\UICore\UISection;
+	use phpHTML\UICore\UISpan;
 
 ?>
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="styles/styles.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="robots" content="noindex">
@@ -32,7 +34,7 @@
 				'<a name="travel">',
 				new UISection([
 					new UIDiv([
-						new UIDiv([new UIGlyphicon(UIGlyphicon::PLANE), new UIGlyphicon(UIGlyphicon::ROAD), new UIGlyphicon(UIGlyphicon::PRINT_), new UIGlyphicon(UIGlyphicon::CAMERA)], 'icons')
+						new UIDiv([new UISpan([], ['glyphicon', 'fa', 'fa-plane']),new UISpan([], ['glyphicon', 'fa', 'fa-ship']),new UISpan([], ['glyphicon', 'fa', 'fa-subway']),new UISpan([], ['glyphicon', 'fa', 'fa-taxi']),new UISpan([], ['glyphicon', 'fa', 'fa-bus'])], 'icons')
 					], 'container')
 				], 'section1'),
 				'</a>'
